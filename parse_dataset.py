640f68858a5a7679a874d1390a7ce78484144058
Atul1710 <atulsureshkumar@gmail.com>
2021-09-22 17:31:07 +0530

    Moved games to games dir, check comments in code for further info.

--- parse_dataset.py
+++ parse_dataset.py
@@ -1,32 +1,36 @@
 import chess.pgn
 import sys
+import xlsxwriter
+import pandas as pd
 
-pgn = open(sys.argv[1])
+pgn = open('games/'+sys.argv[1])
 
-# first_game = chess.pgn.read_game(pgn)
-
-# print(first_game, end="\n\n")
-
-# board = first_game.board()
-
-# first = first_game.next()
-# print(first)
 
 movelist = list()
+totalMovesList = list(list())
 
 game = chess.pgn.read_game(pgn)
 import time
 while game is not None:
-    # print(game.headers)
     white = game.headers["White"]
     black = game.headers["Black"]
-    date_played = game.headers["Date"]
+    datePlayed = game.headers["Date"]
     winner = "White" if game.headers["Result"][0] == '1' else "Black"
-    time_control = game.headers["TimeControl"]
+    timeControl = game.headers["TimeControl"]
     opening = game.headers["Opening"]
+    event = game.headers["Event"]
+    blackElo = game.headers["BlackElo"]
+    whiteElo = game.headers["WhiteElo"]
+    termination = game.headers["Termination"] 
+    variant = game.headers["Variant"]
+    
 
+    detailsList = [white, black, datePlayed, winner, timeControl, opening, blackElo, whiteElo, termination, variant]
+    moveString = ""
     for move in game.mainline():
+        #detailsList.append(move.san())
         movelist.append(move.san())
+        moveString += move.san() + " "
         # white_move = move[:2]
         # black_move = move[2:]    
         # if white:
@@ -38,12 +42,38 @@ while game is not None:
         
         # board.push(move)
         # print(board, end="\n\n")
-    print("White: ", white)
-    print("Black: ", black)
-    print("Time Control: ", time_control)
-    print("Date: ", date_played)
-    print("Winner: ", winner)
-    print("Opening: ", opening)
-    print('\n'.join(movelist))
-    game = chess.pgn.read_game(pgn)
+    
+    # print("White: ", white)
+    # print("Black: ", black)
+    # print("Time Control: ", timeControl)
+    # print("Date: ", datePlayed)
+    # print("Winner: ", winner)
+    # print("Opening: ", opening)
+    # print('\t'.join(movelist))
+    
+    # totalMovesList.append(movelist)
+    detailsList.append(moveString)
+    print(detailsList)
+    
+    #Below code printed each value of the list in a different row so manually wrote each value in columns(see next snippet)
+    # with xlsxwriter.Workbook('test.xlsx') as workbook:
+    #     worksheet = workbook.add_worksheet()
+    #     for row_num, data in enumerate(detailsList):
+    #         worksheet.write_row(row_num, 0, data)
+    
+    #writes pgn file in a row, next functionality to be added - write every pgn file in the games directory in a different row, 
+    #below snippet will only continue writing in 0th row
+    with xlsxwriter.Workbook('test.xlsx') as workbook:
+        column = 0
+        worksheet = workbook.add_worksheet()
+        for data in detailsList:
+            worksheet.write(0, column, data)
+            column += 1
+
+        
     break 
+
+
+#dont mind this 
+# df = pd.DataFrame(detailsList, columns = ["white", "black", "datePlayed", "winner", "timeControl", "opening", "blackElo", "whiteElo", "termination", "variant", "moves"])
+# df.to_excel("pandasTest.xlsx", index = False)

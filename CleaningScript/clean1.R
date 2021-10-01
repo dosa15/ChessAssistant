348b7d8fb0deeccab23ddacd9cf20f6737c87acf
Atul1710 <atulsureshkumar@gmail.com>
2021-10-01 11:17:39 +0530

    cleaning script and cleaned dataset pushed

---
+++ CleaningScript/clean1.R
@@ -0,0 +1,25 @@
+chessData<-read.csv("ChessDataset.csv")
+chessData
+cleanChessData<-chessData[chessData$Variant == "Standard", ]
+
+cleanChessData$White[cleanChessData$White == "Anonymous"]<-"manoghn"
+cleanChessData$Black[cleanChessData$Black == "Anonymous"]<-"manoghn"
+
+cleanChessData$Black.ELO[cleanChessData$Black.ELO == '?']<-1200
+cleanChessData$White.ELO[cleanChessData$Black.ELO == '?']<-1200
+
+cleanChessData$Num.Moves
+
+for(i in 1:nrow(cleanChessData)){
+  cleanChessData[i, "Num.Moves"] = length(unlist(strsplit(cleanChessData[i, "Game.Movelist"], ",")))
+}
+
+cleanChessData[cleanChessData$Num.Moves < 10 & cleanChessData$Termination == "Time Forfeit", ] <- NA
+na.omit(cleanChessData)
+
+
+#excludes<-cleanChessData[cleanChessData$Num.Moves < 10 & cleanChessData$Termination == "Time Forfeit", ]
+#anti_join(cleanChessData, excludes)
+
+cleanChessData
+write.csv(cleanChessData, "D:/sem5/Project/ChessAssistantGithub/ChessAssistant/CleanedChessDataset.csv", row.names=FALSE)

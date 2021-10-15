13b01a79c1eeec3c0ad15614aff123e4b473cf40
Atul Suresh Kumar <atulsureshkumar@gmail.com>
2021-10-15 18:22:36 +0530

    UI Fixed, enhancement required

---
+++ app.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(rchess)
+
+
+
+ui<-
+  fluidPage(
+    mainPanel(
+      htmlTemplate("index.html")
+    )
+)
+  
+# Run the app ----
+#shinyApp(ui = htmlTemplate("index.html"), server = server)
+shinyApp(ui = ui, server = server)
